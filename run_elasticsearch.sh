aa372c5b5ebfe69713c31e8f5a17bafad834a362
euiyounghwang <marieuig@gmail.com>
2023-07-24 12:40:50 -0500

    Update

--- run_elasticsearch.sh
+++ run_elasticsearch.sh
@@ -2,6 +2,10 @@
 docker run --name es8-run -p 9209:9200 -p 9309:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xms1g -Xmx1g" docker.elastic.co/elasticsearch/elasticsearch:8.7.0
 docker run --name kibaba-run -e "ELASTICSEARCH_URL=http://host.docker.internal:9209" -e "ES_JAVA_OPTS=-Xms1g -Xmx1g" -e "ELASTICSEARCH_HOSTS=http://host.docker.internal:9209" -p 5801:5601 docker.elastic.co/kibana/kibana:8.7.0
 
+# docker image tag 4766cb8b195e vagrant:local
+# docker ps -a
+# docker rename Vagrant_New_default_1690219886 Vagrant_Omnisearch
+
 # http://127.0.0.1:8000/docs
 # uvicorn app:app --reload
 
