05992c28a31f8eb7cc30e432f697e1b102a64423
euiyounghwang <marieuig@gmail.com>
2023-08-04 18:25:47 -0500

    Add docker install

---
+++ run_docker_application.sh
@@ -0,0 +1,4 @@
+
+# Postgres Docker
+# docker run -it --rm --name postgres-local -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+docker run  --name postgres-local -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
